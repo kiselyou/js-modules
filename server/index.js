3d934759637fecb610f8ebcb41f6916a35136908
Valery Kiseliou <v.a.kiselyov@gmail.com>
2018-04-13 23:58:14 +0300

    Update server

--- server/index.js
+++ server/index.js
@@ -2,14 +2,13 @@ import path from 'path'
 import express from 'express'
 
 const app = express();
+const publicCatalog = path.join(__dirname + '/../src')
 
-app.use('/src', express.static(__dirname + '/src'));
+app.use('/src', express.static(publicCatalog));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
---
+++ src/test.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ubuntu on 13.04.18.
+ */
+alert(1)
